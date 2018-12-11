ba14d1c1a3f9652364b1393dc05b9df6126719dc
Pranalee Jadhav <pjadhav5@uncc.edu>
2018-12-11 06:50:13 -0500

    Initial Commit

---
+++ MVVM_WeatherApplication/Controllers/AddCityViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddCityViewController.swift
+//  MVVM_WeatherApplication
+//
+//  Created by Pranalee Jadhav on 12/11/18.
+//  Copyright © 2018 Pranalee Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class AddCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save() {
+        
+    }
+
+}
